81f339ecb201bc43456ab9f3f16eb47803098732
Денис Смирнов <smirnovdenys88@gmail.com>
2022-02-12 14:47:54 +0200

    init config

---
+++ config-server/src/test/java/com/nd/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nd.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
